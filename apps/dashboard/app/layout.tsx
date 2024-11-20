4c1e9f217c3f24a970e998d10f4c96714d1497a5
noreplydev <contact.cristiansanchez@gmail.com>
2024-11-20 06:01:11 +0100

    Fix double scroll on homepage

--- apps/dashboard/app/layout.tsx
+++ apps/dashboard/app/layout.tsx
@@ -17,12 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={syne.className}>
+      <body className={syne.className + " relative h-screen w-screen overflow-hidden"}>
         <div
           className={`h-fit w-full flex px-10 py-3 bg-[#131313] 
             border-solid border-[#2D2D2D] border-[1px] font-thin text-center 
-            justify-center items-center text-xl `
-            + jetbrains.className}
+            justify-center items-center text-xl ` + jetbrains.className}
         >🚧 actively building alpha-v0.1</div>
         {children}
       </body>
